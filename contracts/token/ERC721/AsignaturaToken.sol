e2c34f6b0070cc82fe6bf4d963ae3cf90028711f
Javier Montesinos <fj.montesinos@gmail.com>
2020-03-15 01:26:09 +0100

    Updated. Añadida la asignatura como valor al evento de matricular a un alumno en una asignatura

--- contracts/token/ERC721/AsignaturaToken.sol
+++ contracts/token/ERC721/AsignaturaToken.sol
@@ -30,7 +30,8 @@ contract AsignaturaToken is ERC721Metadata {
     mapping (address => address) private _universidadesProfesores;
 
     event UniversidadRegistrada(address universidad, address profesor, address asignatura);
-    event AlumnoMatriculado(address universidad, address profesor, address alumno, string cursoAcademico, uint256 matriculaId);
+    event AlumnoMatriculado(address universidad, address profesor, address asignatura,
+                            address alumno, string cursoAcademico, uint256 matriculaId);
 
     constructor(string memory name, string memory symbol, uint256 creditos, uint256 experimentabilidad, address owner, address estadoAddress)
         ERC721Metadata(name, symbol)
@@ -114,13 +115,16 @@ contract AsignaturaToken is ERC721Metadata {
 
         uint256 matriculaId = mintMatricula(universidad, _universidadesProfesores[universidad], msg.sender, cursoAcademico);
 
-        emit AlumnoMatriculado(universidad, _universidadesProfesores[universidad], msg.sender, cursoAcademico, matriculaId);
+        emit AlumnoMatriculado(universidad, _universidadesProfesores[universidad], address(this), msg.sender, cursoAcademico, matriculaId);
 
         // crear la matricula
         return matriculaId;
     }
 
-    function getMatricula(uint256 matriculaId) public view returns (address universidad, address profesor, address alumno, uint256 nota, bool aprobado, bool evaluado) {
+    function getMatricula(uint256 matriculaId) public view returns (address universidad,
+                                                                    address profesor, address alumno,
+                                                                    uint256 nota, bool aprobado,
+                                                                    bool evaluado) {
         require(_matriculas[matriculaId].valida, 'Matriculo no registrada');
         return (_matriculas[matriculaId].universidad, _matriculas[matriculaId].profesor, _matriculas[matriculaId].alumno,
             _matriculas[matriculaId].nota, _matriculas[matriculaId].aprobado, _matriculas[matriculaId].evaluado);
